fd98b6240ae23e42020dca90ee9b291e39bc4c6b
Hugo <hugo.umbert@estiam.com>
2022-01-25 10:19:25 +0100

    Connect Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'easy-cloud';
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAgP1kjxwBo4ZV5_pAcVIQDMKQbi4ha7fo",
+      authDomain: "easycloud-31b61.firebaseapp.com",
+      projectId: "easycloud-31b61",
+      storageBucket: "easycloud-31b61.appspot.com",
+      messagingSenderId: "276184904347",
+      appId: "1:276184904347:web:b4d2de152e39a37d3ccc3e"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
